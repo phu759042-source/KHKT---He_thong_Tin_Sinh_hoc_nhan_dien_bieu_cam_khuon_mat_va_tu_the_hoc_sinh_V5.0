9260c5957b070b239d4353e4814b57de5386eb8a
Lilvistelm <phu759042@gmail.com>
2026-03-23 15:38:06 +0700

    Update build-he-thong-tin-sinh-hoc.bat

--- build-he-thong-tin-sinh-hoc.bat
+++ build-he-thong-tin-sinh-hoc.bat
@@ -5,10 +5,11 @@ pyinstaller ^
   --icon="Emotion + Posture Detector v5.0.ico" ^
   --add-data "haarcascade_frontalface_default.xml;." ^
   --add-data "emotion_detection.h5;." ^
-  --add-data "Emotion + Posture Detector v5.0.ico;." ^
   --add-data "static;static" ^
+  --add-data "Emotion + Posture Detector v5.0.ico;." ^
   --add-data "Emotion + Posture Detector v3.0 Camera.ico;." ^
   --add-data "Emotion + Posture Detector v3.0 Fullscreen Capture.ico;." ^
+  --add-data "not_enough_time.ico;." ^
   --add-data "ARIALBD 1.ttf;." ^
   --add-data "C:\Users\ADMIN\AppData\Local\Programs\Python\Python310\Lib\site-packages\mediapipe\modules;mediapipe/modules" ^
   emotion_posture_detector_v5.0.py
